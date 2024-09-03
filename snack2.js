799ed77deef9355b0aab4a676f1a0141a7a3a5b1
GiorgioDordo <djole6789@gmail.com>
2024-09-03 17:31:56 +0200

    printed all the names uppercase

--- snack2.js
+++ snack2.js
@@ -8,4 +8,10 @@ const students = [
     {Id:102, name: 'Piero della Francesca', grades: 50},
     {Id:120, name: 'Francesca da Polenta', grades: 84}
     ]
-
+    
+// todo students name uppercase
+    const studentsName = students.map((student) => {
+        const studentsName = student.name.toUpperCase();
+        return studentsName;
+    });
+    console.log(studentsName);
